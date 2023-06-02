ffb975fd8d8c500767a60539c607d214bb892123
Charlie Boggus <charlie.boggus@gmail.com>
2023-06-02 13:43:22 -0400

    build out rough framework of the about page

---
+++ src/components/AboutFaqItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aboutFaqItem">
+    <el-text class="question">{{ props.question }}?</el-text>
+    <br />
+    <el-text class="answer">{{ props.answer }}</el-text>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+})
+</script>
+
+<style scoped>
+.aboutFaqItem {
+  margin-bottom: 20px;
+}
+
+.question {
+  font-weight: bold;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,9 +9,45 @@
     <br />
     <!-- Page content -->
     <el-row>
-      <el-text style="text-align: center">Under construction</el-text>
+      <el-col :span="2" />
+      <el-col :span="20">
+        <div class="collapse-menu">
+          <el-collapse v-model="active">
+            <el-collapse-item title="Background" name="1">
+              <div class="background">
+                <about-background />
+              </div>
+            </el-collapse-item>
+            <el-collapse-item title="Architecture" name="2">
+              <div class="architecture">
+                <about-architecture />
+              </div>
+            </el-collapse-item>
+            <el-collapse-item title="FAQ" name="3">
+              <div v-for="item in loadedFaq" :key="item.question">
+                <about-faq-item
+                  :question="item.question"
+                  :answer="item.answer"
+                />
+              </div>
+            </el-collapse-item>
+          </el-collapse>
+        </div>
+      </el-col>
+      <el-col :span="2" />
     </el-row>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import AboutArchitecture from '@/components/AboutArchitecture.vue'
+import AboutBackground from '@/components/AboutBackground.vue'
+import AboutFaqItem from '@/components/AboutFaqItem.vue'
+import faq from '@/assets/json/faq.json'
+
+const active = ref(['0'])
+const loadedFaq = ref(faq)
+</script>
+
+<style scoped></style>
